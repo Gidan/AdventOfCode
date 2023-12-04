accd870e02dd8ea1dcf6ab31cd8183ac04ecbde0
Andrea Amatucci <amatucci.andrea@gmail.com>
2023-12-04 21:53:00 +0100

    refactor: Puzzle3

--- src/main/kotlin/Puzzle3.kt
+++ src/main/kotlin/Puzzle3.kt
@@ -7,6 +7,9 @@ fun main() {
 
 typealias Coordinates = Pair<Int, Int>
 
+data class CheckResult(val symbolFound: Boolean, val starFound: Boolean)
+data class CheckLineResult(val symbolFound: Boolean, val gearPosition: Int)
+
 class Puzzle3 : Puzzle() {
 
     override fun getPuzzleNumber(): Int = 3
@@ -16,11 +19,11 @@ class Puzzle3 : Puzzle() {
         val width = lines[0].length
         val height = lines.size
 
-        val checkRight: (String, Int) -> Pair<Boolean, Boolean> =
-            { line, i -> Pair(i < width && line[i] != '.', if (i < width) line[i] == '*' else false) }
-        val checkLeft: (String, Int) -> Pair<Boolean, Boolean> =
-            { line, i -> Pair(i - 1 > 0 && line[i] != '.', if (i - 1 > 0) line[i] == '*' else false) }
-        val checkRowInRange: (Int, Int, Int) -> Pair<Boolean, Int> = { lineIndex, start, end ->
+        val checkRight: (String, Int) -> CheckResult =
+            { line, i -> CheckResult(i < width && line[i] != '.', if (i < width) line[i] == '*' else false) }
+        val checkLeft: (String, Int) -> CheckResult =
+            { line, i -> CheckResult(i - 1 > 0 && line[i] != '.', if (i - 1 > 0) line[i] == '*' else false) }
+        val checkRowInRange: (Int, Int, Int) -> CheckLineResult = { lineIndex, start, end ->
             if (lineIndex in 0..<height) {
                 val l = lines[lineIndex]
                 val substring = l.substring(start, end)
@@ -30,9 +33,9 @@ class Puzzle3 : Puzzle() {
                 } else {
                     gearIndex = -1
                 }
-                Pair(substring.any { it != '.' && !it.isDigit() }, gearIndex)
+                CheckLineResult(substring.any { it != '.' && !it.isDigit() }, gearIndex)
             } else {
-                Pair(false, -1)
+                CheckLineResult(false, -1)
             }
         }
 
@@ -44,16 +47,16 @@ class Puzzle3 : Puzzle() {
         for (lineIdx in lines.indices) {
             val line = lines[lineIdx]
             var i = 0
-            var numStr = ""
+            var numLiteral = ""
             while (i < width) {
                 if (line[i].isDigit()) {
-                    numStr += line[i]
+                    numLiteral += line[i]
                     var digitIdx = i + 1
                     while (digitIdx < width && line[digitIdx].isDigit()) {
-                        numStr += line[digitIdx]
+                        numLiteral += line[digitIdx]
                         digitIdx++
                     }
-                    val num = numStr.toInt()
+                    val num = numLiteral.toInt()
 
                     val symbolRight = checkRight(line, digitIdx)
                     val symbolLeft = checkLeft(line, i - 1)
@@ -68,38 +71,35 @@ class Puzzle3 : Puzzle() {
                         min(digitIdx + 1, width - 1)
                     )
 
-                    if (symbolRight.first or symbolLeft.first or symbolAbove.first or symbolBelow.first) {
+                    if (symbolRight.symbolFound or symbolLeft.symbolFound or symbolAbove.symbolFound or symbolBelow.symbolFound) {
                         //symbol found
                         sum += num
-                        //println("adding $num")
                     }
 
-                    if (symbolRight.second) {
-                        val gearCoord = Pair(lineIdx, digitIdx)
-                        val gearList = gearMap.getOrDefault(gearCoord, mutableListOf())
+                    val addGear: (Coordinates) -> Unit = {
+                        val gearList = gearMap.getOrDefault(it, mutableListOf())
                         gearList.add(num)
-                        gearMap[gearCoord] = gearList
-                    }
-                    if (symbolLeft.second) {
-                        val gearCoord = Pair(lineIdx, i - 1)
-                        val gearList = gearMap.getOrDefault(gearCoord, mutableListOf())
-                        gearList.add(num)
-                        gearMap[gearCoord] = gearList
-                    }
-                    if (symbolAbove.second >= 0) {
-                        val gearCoord = Pair(lineIdx - 1, symbolAbove.second)
-                        val gearList = gearMap.getOrDefault(gearCoord, mutableListOf())
-                        gearList.add(num)
-                        gearMap[gearCoord] = gearList
-                    }
-                    if (symbolBelow.second >= 0) {
-                        val gearCoord = Pair(lineIdx + 1, symbolBelow.second)
-                        val gearList = gearMap.getOrDefault(gearCoord, mutableListOf())
-                        gearList.add(num)
-                        gearMap[gearCoord] = gearList
+                        gearMap[it] = gearList
                     }
 
-                    numStr = ""
+                    if (symbolRight.starFound) {
+                        val gearPosition = Pair(lineIdx, digitIdx)
+                        addGear(gearPosition)
+                    }
+                    if (symbolLeft.starFound) {
+                        val gearPosition = Pair(lineIdx, i - 1)
+                        addGear(gearPosition)
+                    }
+                    if (symbolAbove.gearPosition >= 0) {
+                        val gearPosition = Pair(lineIdx - 1, symbolAbove.gearPosition)
+                        addGear(gearPosition)
+                    }
+                    if (symbolBelow.gearPosition >= 0) {
+                        val gearPosition = Pair(lineIdx + 1, symbolBelow.gearPosition)
+                        addGear(gearPosition)
+                    }
+
+                    numLiteral = ""
                     i = digitIdx
                 } else {
                     i++
@@ -108,8 +108,8 @@ class Puzzle3 : Puzzle() {
 
         }
 
-        val filter = gearMap.filter { (symbolPosition, numberList) -> numberList.size == 2 }
-        filter.forEach { (t, u) -> gearRatio += u.reduce { acc, i -> acc * i } }
+        val gearsWithTwoNumbers = gearMap.filter { (_, numberList) -> numberList.size == 2 }
+        gearsWithTwoNumbers.forEach { (_, n) -> gearRatio += n.reduce { acc, i -> acc * i } }
 
         println("part1: $sum")
         println("part2: $gearRatio")
